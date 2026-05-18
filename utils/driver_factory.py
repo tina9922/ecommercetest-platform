5540fa11dd2c88a90ee56c47f838727c21858e04
Vasu Agarwal <agarwalvasundhara537@gmail.com>
2026-05-18 14:36:23 -0700

    github actions

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -11,6 +11,7 @@ def get_driver():
     # Chrome setup
     # Headless browser mode
     options.add_argument("--headless")
+    print("trigger CI")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options = webdriver.ChromeOptions()
@@ -24,3 +25,60 @@ def get_driver():
     options.add_argument("--start-maximized")
     driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()),options=options)
     return driver 
+'''
+    Think of regression.yml as:
+
+📋  Instructions for GitHub Actions telling GitHub:
+    Whenever code changes happen, automatically run my automation tests.
+
+    Artifacts =
+👉 downloadable files after pipeline completes
+
+  Example:
+
+  HTML reports
+  screenshots
+  logs
+
+  Your regression.yml is basically:
+
+👉 “automation instructions for GitHub cloud machines.”
+
+  The regression.yml file defines the CI/CD workflow for the automation framework using GitHub Actions. 
+  It automatically triggers test execution whenever code is pushed or a pull request is created on the main branch. 
+  The workflow provisions a Linux runner(UBUNTU VM), installs Python and project dependencies, downloads your repository code into the VM
+  executes the pytest regression suite, and uploads the generated test reports as artifacts for analysis.
+
+    End-to-End Flow
+
+When you push code:
+
+Developer Pushes Code
+        ↓
+GitHub Actions Triggered
+        ↓
+Ubuntu VM Created
+        ↓
+Repo Downloaded
+        ↓
+Python Installed
+        ↓
+Dependencies Installed
+        ↓
+Pytest Runs
+        ↓
+Reports Generated
+        ↓
+Artifacts Uploaded
+
+Now you need to verify:
+👉 “Does the pipeline actually run successfully in GitHub?”
+
+As soon as push completes:
+
+👉 GitHub detects:
+
+.github/workflows/regression.yml
+
+and automatically triggers pipeline.
+'''
