556f330b99302b563260d68c4cacce599fd42bc3
Vasu Agarwal <agarwalvasundhara537@gmail.com>
2026-05-18 14:28:31 -0700

    trigger github actions

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -4,6 +4,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 import tempfile
 
 def get_driver():
+    # Needed for GitHub Actions 
+    # Your tests may fail initially because GitHub Actions Linux runners often need:
+    # Without headless mode:
+    # ❌ GitHub Actions may fail launching browser
+    # Chrome setup
+    # Headless browser mode
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options = webdriver.ChromeOptions()
     options.add_argument("--incognito")
     options.add_argument("--disable-extensions")
